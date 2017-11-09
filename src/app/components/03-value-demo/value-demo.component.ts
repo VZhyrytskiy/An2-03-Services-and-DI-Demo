91f4d4606c411bf9467952da319dd6ad85cd66de
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-11-09 17:31:11 +0200

    project was updated to angular v5

--- src/app/components/03-value-demo/value-demo.component.ts
+++ src/app/components/03-value-demo/value-demo.component.ts
@@ -1,11 +1,6 @@
-import { Component, OnInit, OpaqueToken, Inject, InjectionToken } from '@angular/core';
+import { Component, OnInit, Inject, InjectionToken } from '@angular/core';
 
 import { DataService } from './../../services';
-
-// Deprecation Notes about OpaqueToken
-// since v4.0.0 because it does not support type information,
-// use InjectionToken<?> instead.
-// const TITLE = new OpaqueToken('title');
 
 const TITLE = new InjectionToken<string>('title');
 const dsInst = new DataService();
--- src/app/services/data-top-n.factory.ts
+++ src/app/services/data-top-n.factory.ts
@@ -1,8 +1,6 @@
-import { OpaqueToken, InjectionToken } from '@angular/core';
+import { InjectionToken } from '@angular/core';
 import { DataService } from '.';
 
-// OpaqueToken is deprecated since v4.0.0
-// export const Data_Top3 = new OpaqueToken('DataTop3');
 export const Data_Top3 = new InjectionToken<any[]>('DataTop3');
 
 export function DataTopNFactory(take: number) {
