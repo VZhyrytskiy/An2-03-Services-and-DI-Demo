f1b1357b4f611e985840f1eeff1b26d6511db53b
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-11-10 11:32:23 +0200

    updated useExisting example

--- src/app/components/05-existing-demo/existing-demo.component.ts
+++ src/app/components/05-existing-demo/existing-demo.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
-import { LoggerService, MyLoggerService } from './../../services';
+import { LoggerService, MyLoggerService, DataService } from './../../services';
 
 @Component({
   selector: 'app-existing-demo',
   templateUrl: './existing-demo.component.html',
   styleUrls: ['./existing-demo.component.css'],
   providers: [
-    { provide: LoggerService, useClass: LoggerService },
-    { provide: MyLoggerService, useExisting: LoggerService }
+    LoggerService,
+    { provide: DataService, useClass: LoggerService }, // copy of LoggerService
+    { provide: MyLoggerService, useExisting: LoggerService } // the same as LoggerService
   ]
 })
 export class ExistingDemoComponent implements OnInit {
 
  constructor(
+    private loggerService: LoggerService,
+    private logService: DataService,
     private myLoggerService: MyLoggerService
   ) {}
 
   ngOnInit() {
     console.log('useExisting:');
+    console.log(Object.is(this.loggerService, this.myLoggerService));
+    console.log(Object.is(this.logService, this.myLoggerService));
     this.myLoggerService.log(123);
   }
 
