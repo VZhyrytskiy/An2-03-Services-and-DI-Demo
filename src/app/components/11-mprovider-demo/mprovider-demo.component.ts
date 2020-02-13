0bf8e5c712fc8041987b7da3214c498cd8bc6c35
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-02-13 16:40:50 +0200

    minor fix

--- src/app/components/11-mprovider-demo/mprovider-demo.component.ts
+++ src/app/components/11-mprovider-demo/mprovider-demo.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit, InjectionToken, Inject } from '@angular/core';
 
-const MyToken = new InjectionToken<string[]>('token');
+// const token = new InjectionToken<any[]>('token');
+export class Token {}
+
 
 @Component({
   selector: 'app-mprovider-demo',
   templateUrl: './mprovider-demo.component.html',
   styleUrls: ['./mprovider-demo.component.css'],
   providers: [
-    { provide: MyToken, useValue: '1', multi: true },
-    { provide: MyToken, useValue: '2', multi: true }
+    { provide: Token, useValue: 1, multi: true },
+    { provide: Token, useValue: 2, multi: true }
   ]
 })
 export class MProviderDemoComponent implements OnInit {
   content = '';
 
-  constructor(@Inject(MyToken) private services: string[]) {}
+  constructor(@Inject(Token) private services: string[]) {}
 
   ngOnInit() {
     this.content = `${this.services[0]} ${this.services[1]}`;
