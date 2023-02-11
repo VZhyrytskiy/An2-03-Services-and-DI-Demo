5bedffa1dea70a6ea63ab3858ab651e7cb336240
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2023-02-11 11:28:51 +0200

    upd: update Angular to v.15

--- src/app/components/05-providedIn/provided-in-demo-routing.module.ts
+++ src/app/components/05-providedIn/provided-in-demo-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes, {}),
   ],
   exports: [RouterModule]
 })
